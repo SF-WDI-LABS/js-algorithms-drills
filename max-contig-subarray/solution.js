21d1fa4e2f1a02a0fb5678351d6f971522a44e5e
jeanmw <jean.weatherwax@gmail.com>
2017-02-24 19:17:43 -0800

    better solution for max contig subarray

--- max-contig-subarray/solution.js
+++ max-contig-subarray/solution.js
@@ -1,23 +1,17 @@
 function maxContigSubarray(arr) {
-    var maxSum = -10000;
-    var currentSum = 0;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] == 0) {
-            continue;
-        }
-        currentSum = currentSum+arr[i];
-        maxSum = Math.max(currentSum, maxSum);
-        if (currentSum < 0) {
-            currentSum = 0;
-        }
-    }
-    return maxSum;
+  var currentMax = 0;
+  var max = 0;
+  for (var i = 0; i < arr.length; i += 1) {
+    currentMax = Math.max(0, currentMax + arr[i]);
+    max = Math.max(max, currentMax);
+  }
+  return max;
 }
 
 testArr1 = [4,7,-1,-12,7];
 testArr2 = [0,8,0,2,3];
 testArr3 = [-1,5,66,7,-8,-22,4];
-testArr4 = [-2,-3,4,-1,-5,6];
+testArr4 = [-2,-3,4,1,-1,-5,4];
 
 console.log(maxContigSubarray(testArr1));
 console.log(maxContigSubarray(testArr2));
