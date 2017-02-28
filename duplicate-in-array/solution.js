a891e2022b0902a8778e52b9686af3ae898a48dc
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-02-28 11:43:54 -0800

    Update solution.js

--- duplicate-in-array/solution.js
+++ duplicate-in-array/solution.js
@@ -1,8 +1,9 @@
+
 function findDupsInArray(arr) {
     var sorted_arr = arr.sort();
     var results = [];
     for (var i = 0; i < arr.length - 1; i++) {
-        if (sorted_arr[i + 1] == sorted_arr[i]) {
+        if (sorted_arr[i + 1] === sorted_arr[i]) {
             results.push(sorted_arr[i]);
         }
     }
@@ -11,5 +12,5 @@ function findDupsInArray(arr) {
 
 var arr1 = [7,7,8,1,14,2,2,-5,6,0];
 var arr2 = [-9,8,7,3,4,4,-1,-1,7];
-console.log(removeDupsInArray(arr1));
-console.log(removeDupsInArray(arr2));
+console.log(findDupsInArray(arr1));
+console.log(findDupsInArray(arr2));
