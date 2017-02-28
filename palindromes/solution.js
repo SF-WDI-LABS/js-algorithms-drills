f90be5902bb0a8cf864a3d171f876097620d9922
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-02-28 08:53:01 -0800

    indentation fix

--- palindromes/solution.js
+++ palindromes/solution.js
@@ -1,12 +1,12 @@
 function isWordAPalindrome(input) {
-	var isPalindrome = true;
+  var isPalindrome = true;
   var word = input.toLowerCase();
-	for (var i=1; i < word.length+1; i++) {
-		if (word.substring(i-1, i) != word.substring(word.length-i, word.length+1-i)) {
-				isPalindrome = false;
-		}
-		return isPalindrome;
+  for (var i=1; i < word.length+1; i++) {
+	if (word.substring(i-1, i) != word.substring(word.length-i, word.length+1-i)) {
+		isPalindrome = false;
 	}
+	return isPalindrome;
+   }
 }
 
 console.log(isWordAPalindrome("Hannah"));
