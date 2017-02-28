5ecffa44aaa2cad9c43db286d0206177995b156f
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-02-28 10:07:28 -0800

    Update solution.js

--- palindromes/solution.js
+++ palindromes/solution.js
@@ -1,14 +1,19 @@
 function isWordAPalindrome(input) {
-  var isPalindrome = true;
   var word = input.toLowerCase();
-  for (var i=1; i < word.length+1; i++) {
-	if (word.substring(i-1, i) != word.substring(word.length-i, word.length+1-i)) {
-		isPalindrome = false;
+  for (var i=0; i < input.length; i++) {
+	if (word[i] !== word[input.length-1-i]) {
+		return false;
 	}
-	return isPalindrome;
+	return true;
    }
 }
 
 console.log(isWordAPalindrome("Hannah"));
 console.log(isWordAPalindrome("racecar"));
 console.log(isWordAPalindrome("Blarg"));
+
+//alternate
+function isWordAPalindrome(input) {
+  return input === input.split('').reverse().join('');
+}
+
