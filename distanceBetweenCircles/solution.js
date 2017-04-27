f36a9fd6ebb0fd2e94f65c1be2ad2efc475bc965
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-04-27 09:37:25 -0700

    Update solution.js

--- distanceBetweenCircles/solution.js
+++ distanceBetweenCircles/solution.js
@@ -1,4 +1,4 @@
-  function distanceBetweenEdges(a,b) {
+  function distanceBetweenCircles(a,b) {
     let distanceBetweenCenters = Math.sqrt(Math.abs(Math.pow((a.center.x - b.center.x), 2) + Math.pow((a.center.y - b.center.y), 2)));
     let distanceBetweenEdges = distanceBetweenCenters - a.radius - b.radius;
     return distanceBetweenEdges < 0 ? 0 : distanceBetweenEdges;
