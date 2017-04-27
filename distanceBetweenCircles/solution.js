dcdd66897373fcc9d3fc63d0b914d49e891859a9
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-04-27 09:47:30 -0700

    Update solution.js

--- distanceBetweenCircles/solution.js
+++ distanceBetweenCircles/solution.js
@@ -3,3 +3,33 @@
     let distanceBetweenEdges = distanceBetweenCenters - a.radius - b.radius;
     return distanceBetweenEdges < 0 ? 0 : distanceBetweenEdges;
   }
+
+
+
+var circle1 = {
+  center : {
+    x: 3,
+    y: 4
+  },
+  radius: 2
+};
+
+var circle2 = {
+  center : {
+    x: 14,
+    y: 11,
+  },
+  radius: 8
+};
+
+var circle3 = {
+  center: {
+    x:2,
+    y:2,
+  },
+  radius: 5
+};
+
+console.log(distanceBetweenCircles(circle1, circle2));//should be 3.0384048104052983
+console.log(distanceBetweenCircles(circle1, circle3)); //should be 0
+
