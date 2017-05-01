1dd8971d232350c25a77195e1eb16f1357e5eeb4
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-05-01 09:56:10 -0700

    Update solution.js

---
+++ largest-square-in-circle/solution.js
@@ -0,0 +1,7 @@
+function largestSquareAreaInCircle(r) {
+  return 2 * (Math.pow(r,2));
+}
+
+console.log(largestSquareAreaInCircle(5)); //50
+console.log(largestSquareAreaInCircle(7)); //98
+console.log(largestSquareAreaInCircle(12)); //288
