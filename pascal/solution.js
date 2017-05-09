944c9440efa90f19907df571ef02bc0cb5049fba
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-05-09 15:46:58 -0700

    Update solution.js

--- pascal/solution.js
+++ pascal/solution.js
@@ -12,7 +12,7 @@ function pascal(n) {
   return result;
 }
 
-//helper to print out meaninful result
+//helper to display meaninful result
 function printPascal(n) {
   for (var i = 0; i < pascal(n).length; i++){
     document.write(pascal(n)[i]+"<br>");
