0930cb047133eb0e157ba4d682b56cb370e02239
Jean Weatherwax <jeanmw@users.noreply.github.com>
2017-05-09 15:47:15 -0700

    Update starter-code.js

--- pascal/starter-code.js
+++ pascal/starter-code.js
@@ -1,7 +1,7 @@
 function pascal(n) {
 }
 
-//helper to print out meaninful result
+//helper to display meaninful result
 function printPascal(n) {
   for (var i = 0; i < pascal(n).length; i++){
     document.write(pascal(n)[i]+"<br>");
