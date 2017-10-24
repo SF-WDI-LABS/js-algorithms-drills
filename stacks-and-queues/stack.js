9ad0b7c99adfa07741360c88f3147cc0c2a9ecdf
Esther S. Weon <esther.s.weon@gmail.com>
2017-10-24 07:01:32 -0700

    Update stack.js

--- stacks-and-queues/stack.js
+++ stacks-and-queues/stack.js
@@ -1,15 +1,20 @@
 module.exports = class {
-    constructor() {
-        this.data = [];
+  constructor(seedData) {
+    this.data = [];
+    if (seedData) {
+      for (const datum of seedData) {
+        this.push(datum);
+      }
     }
-    push(item) {
-    }
-    pop() {
-    }
-    peek() {
-    }
-    clear() {
-    }
-    length() {
-    }
+  }
+  push(item) {
+  }
+  pop() {
+  }
+  peek() {
+  }
+  clear() {
+  }
+  length() {
+  }
 }
